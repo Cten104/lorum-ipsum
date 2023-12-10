47a4b048642de032ed4014acfcd9fe1971f296aa
David <dg14985p@pace.edu>
2023-12-10 17:20:01 -0500

    darmode banner changes

--- assets/JS/darkmode.js
+++ assets/JS/darkmode.js
@@ -1,5 +1,17 @@
 const chk = document.getElementById('chk');
+const bannerImage = document.getElementById('banner');
+const bannerDarkImage = document.getElementById('bannerDark');
 
 chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+    // Toggle dark mode class on body
+    document.body.classList.toggle('dark');
+
+    // Toggle visibility of banners based on dark mode
+    if (document.body.classList.contains('dark')) {
+        bannerImage.style.display = 'none';
+        bannerDarkImage.style.display = 'inline';
+    } else {
+        bannerImage.style.display = 'inline';
+        bannerDarkImage.style.display = 'none';
+    }
 });
